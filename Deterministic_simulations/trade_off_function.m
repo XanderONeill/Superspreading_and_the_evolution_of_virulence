984a96491611469166c257f2895252b50291ffb3
XanderONeill <91188472+XanderONeill@users.noreply.github.com>
2025-07-10 14:32:56 +0100

    Delete Deterministic_simulations/trade_off_function.m

--- Deterministic_simulations/trade_off_function.m
+++
@@ -1,6 +0,0 @@
-function f = trade_off_function(alpha, alpha_min, alpha_max)
-a = -0.25;
-beta_min = 2e-05;
-beta_max = 1e-03;
-f = beta_min + ((beta_max - beta_min)*(1 - (alpha_max - alpha)/(alpha_max - alpha_min)))./(1 + a*(alpha_max - alpha)/(alpha_max - alpha_min));
-end
